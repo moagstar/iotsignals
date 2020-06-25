40fe7c065492d497ef9eff0f562ed338aa6a4709
Hielke Kramer <kramerh@gmail.com>
2020-06-25 15:48:40 +0200

    Only allow POST and downgrade to django 2 again

--- api/src/passage/tests/test_api.py
+++ api/src/passage/tests/test_api.py
@@ -169,121 +169,24 @@ class PassageAPITestV0(APITestCase):
         res = self.client.post(self.URL, TEST_POST, format='json')
         self.assertEqual(res.status_code, 409, res.data)
 
-    def test_list_passages(self):
-        """ Test listing all passages """
+    def test_get_passages_not_allowed(self):
         PassageFactory.create()
-        res = self.client.get(self.URL)
-
-        self.assertEqual(res.status_code, 200)
-        # in the setup we also create a csv.
-        self.assertEqual(len(res.data['results']), 2)
-
-    def test_get_passage(self):
-        """ Test getting a passage """
-        res = self.client.get('{}{}/'.format(self.URL, self.p.id))
-
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.data['id'], self.p.id)
+        response = self.client.get(self.URL)
+        self.assertEqual(response.status_code, 405)
 
     def test_update_passages_not_allowed(self):
-        """ Test if updating a passage is not allowed """
-        res = self.client.put('{}{}/'.format(
-            self.URL, self.p.id), dict(merk='DummyMerk'))
+        # first post a record
+        self.client.post(self.URL, TEST_POST, format='json')
 
-        self.assertEqual(res.status_code, 405)
+        # Then check if I cannot update it
+        response = self.client.put(f'{self.URL}{TEST_POST["id"]}/', TEST_POST, format='json')
+        self.assertEqual(response.status_code, 405)
 
-        res = self.client.put('{}{}/'.format(
-            self.URL, self.p.id), TEST_POST, format='json')
-
-        self.assertEqual(res.status_code, 405)
 
     def test_delete_passages_not_allowed(self):
-        """ Test if deleting a passage is not allowed """
-        res = self.client.delete('{}{}/'.format(self.URL, self.p.id))
+        # first post a record
+        self.client.post(self.URL, TEST_POST, format='json')
 
-        self.assertEqual(res.status_code, 405)
-
-    def test_default_response_is_json(self):
-        """ Test if the default response of the API is on JSON """
-        res = self.client.get(self.URL)
-        self.valid_response(self.URL, res, 'application/json')
-
-    def test_xml_response(self):
-        """ Test XML response """
-        url = '{}{}'.format(self.URL, '?format=xml')
-        res = self.client.get(url)
-        self.valid_response(url, res, 'application/xml; charset=utf-8')
-
-    def test_csv_response(self):
-        """ Test CSV response """
-        url = '{}{}'.format(self.URL, '?format=csv')
-        res = self.client.get(url)
-        self.valid_response(url, res, 'text/csv; charset=utf-8')
-
-    def test_html_response(self):
-        """ Test HTML response """
-        url = '{}{}'.format(self.URL, '?format=api')
-        res = self.client.get(url)
-        self.valid_response(url, res, 'text/html; charset=utf-8')
-
-    def test_merk_filters(self):
-        """ Test filtering on 'merk'"""
-        # Create two passages with a different 'merk' value
-        passage_ferrari = PassageFactory(merk='ferrari')
-        passage_ferrari.save()
-        passage_fiat = PassageFactory(merk='fiat')
-        passage_fiat.save()
-
-        # Make a request with a merk filter and check if the result is correct
-        url = '{}{}'.format(self.URL, '?merk=ferrari')
-        res = self.client.get(url)
-        self.assertEqual(len(res.data['results']), 1)
-        self.assertEqual(res.data['results'][0]['id'], passage_ferrari.id)
-
-    def test_voertuig_soort_filters(self):
-        """ Test filtering on 'voertuig_soort'"""
-        # Create two passages with a different 'voertuig_soort' value
-        passage_bus = PassageFactory(voertuig_soort='bus')
-        passage_bus.save()
-        passage_fiets = PassageFactory(voertuig_soort='fiets')
-        passage_fiets.save()
-
-        # Make a request with a voertuig_soort filter and
-        # check if the result is
-        # correct
-
-        url = '{}{}'.format(self.URL, '?voertuig_soort=bus')
-        res = self.client.get(url)
-        self.assertEqual(len(res.data['results']), 1)
-        self.assertEqual(res.data['results'][0]['id'], passage_bus.id)
-
-    def test_version_filters(self):
-        """ Test filtering on 'version'"""
-        # Create two passages with a different 'version' value
-        passage_v1 = PassageFactory(version='1')
-        passage_v1.save()
-        passage_v2 = PassageFactory(version='2')
-        passage_v2.save()
-
-        # Make a request with a version filter
-        # and check if the result is correct
-        url = '{}{}'.format(self.URL, '?version=1')
-        res = self.client.get(url)
-        self.assertEqual(len(res.data['results']), 1)
-        self.assertEqual(res.data['results'][0]['id'], passage_v1.id)
-
-    def test_kenteken_land_filters(self):
-        """ Test filtering on 'kenteken_land'"""
-        # Create two passages with a different 'kenteken_land' value
-        passage_nl = PassageFactory(kenteken_land='NL')
-        passage_nl.save()
-        passage_es = PassageFactory(kenteken_land='ES')
-        passage_es.save()
-
-        # Make a request with a kenteken_land filter and check if the result is
-        # correct
-
-        url = '{}{}'.format(self.URL, '?kenteken_land=NL')
-        res = self.client.get(url)
-        self.assertEqual(len(res.data['results']), 1)
-        self.assertEqual(res.data['results'][0]['id'], passage_nl.id)
+        # Then check if I cannot update it
+        response = self.client.delete(f'{self.URL}{TEST_POST["id"]}/')
+        self.assertEqual(response.status_code, 405)
