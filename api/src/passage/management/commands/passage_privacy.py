4957ce13a76e4ac7e72453ba8c80358f5714b87f
Niels Lensink <nielslensink@gmail.com>
2021-06-07 16:22:42 +0200

    Added options to enable updates and vacuum

--- api/src/passage/management/commands/passage_privacy.py
+++ api/src/passage/management/commands/passage_privacy.py
@@ -15,6 +15,11 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--sleep', nargs='?', default=1, type=int)
+        parser.add_argument('--update', dest='update', action='store_true')
+        parser.add_argument('--no-update', dest='update', action='store_false')
+        parser.add_argument('--vacuum', dest='vacuum', action='store_true')
+        parser.add_argument('--no-vacuum', dest='vacuum', action='store_false')
+        parser.set_defaults(update=True, vacuum=True)
 
     def handle(self, **options):
         verbosity = int(options['verbosity'])
@@ -37,51 +42,53 @@ class Command(BaseCommand):
         for date in (
             date_min + timedelta(n) for n in range((date_max - date_min).days)
         ):
-            self.stdout.write(f"Selecting data in: {self.style.SQL_KEYWORD(date)}")
-            num_updated_rows = Passage.objects.filter(
-                passage_at__gte=date,
-                passage_at__lt=date + timedelta(days=1),
-            ).update(
-                datum_eerste_toelating=TruncYear('datum_eerste_toelating'),
-                datum_tenaamstelling=Value(None),
-                toegestane_maximum_massa_voertuig=Case(
-                    When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(1500)),
-                    default=F('toegestane_maximum_massa_voertuig'),
-                ),
-                europese_voertuigcategorie_toevoeging=Case(
-                    When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(None)),
-                    default=F('europese_voertuigcategorie_toevoeging'),
-                ),
-                inrichting=Case(
-                    When(
-                        voertuig_soort__iexact='personenauto',
-                        then=Value('Personenauto'),
+            if options['update']:
+                self.stdout.write(f"Selecting data in: {self.style.SQL_KEYWORD(date)}")
+                num_updated_rows = Passage.objects.filter(
+                    passage_at__gte=date,
+                    passage_at__lt=date + timedelta(days=1),
+                ).update(
+                    datum_eerste_toelating=TruncYear('datum_eerste_toelating'),
+                    datum_tenaamstelling=Value(None),
+                    toegestane_maximum_massa_voertuig=Case(
+                        When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(1500)),
+                        default=F('toegestane_maximum_massa_voertuig'),
                     ),
-                    default=F('inrichting'),
-                ),
-                merk=Case(
-                    When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(None)),
-                    default=F('merk'),
-                ),
-            )
-            self.stdout.write(f'Processed: {self.style.SUCCESS(num_updated_rows)}')
+                    europese_voertuigcategorie_toevoeging=Case(
+                        When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(None)),
+                        default=F('europese_voertuigcategorie_toevoeging'),
+                    ),
+                    inrichting=Case(
+                        When(
+                            voertuig_soort__iexact='personenauto',
+                            then=Value('Personenauto'),
+                        ),
+                        default=F('inrichting'),
+                    ),
+                    merk=Case(
+                        When(toegestane_maximum_massa_voertuig__lte=3500, then=Value(None)),
+                        default=F('merk'),
+                    ),
+                )
+                self.stdout.write(f'Processed: {self.style.SUCCESS(num_updated_rows)}')
 
-            if num_updated_rows > 0:
+                if num_updated_rows > 0:
+                    self.stdout.write(f'sleeping for: {self.style.SUCCESS(sleep)}')
+                    time.sleep(sleep)
+                else:
+                    self.stdout.write(f'sleeping for: {self.style.SUCCESS(0.1)}')
+                    time.sleep(0.1)
+
+            if options['vacuum']:
+                partition_name = f'passage_passage_{date:%Y%m%d}'
+                vacuum_query = f'VACUUM FULL ANALYZE {partition_name}'
+                self.stdout.write(f'Starting vacuum: {vacuum_query}')
+                try:
+                    with connection.cursor() as cursor:
+                        cursor.execute(vacuum_query)
+                except ProgrammingError as e:
+                    self.stderr.write(f'Error vacuuming: {e}')
                 self.stdout.write(f'sleeping for: {self.style.SUCCESS(sleep)}')
                 time.sleep(sleep)
-            else:
-                self.stdout.write(f'sleeping for: {self.style.SUCCESS(0.1)}')
-                time.sleep(0.1)
-
-            partition_name = f'passage_passage_{date:%Y%m%d}'
-            vacuum_query = f'VACUUM FULL ANALYZE {partition_name}'
-            self.stdout.write(f'Starting vacuum: {vacuum_query}')
-            try:
-                with connection.cursor() as cursor:
-                    cursor.execute(vacuum_query)
-            except ProgrammingError as e:
-                self.stderr.write(f'Error vacuuming: {e}')
-            self.stdout.write(f'sleeping for: {self.style.SUCCESS(sleep)}')
-            time.sleep(sleep)
 
         self.stdout.write(self.style.SUCCESS('Finished'))
