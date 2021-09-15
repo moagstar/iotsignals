46d60173be4a79c5ca195f215b270d1b6455b0f4
Daniel Bradburn <moagstar@gmail.com>
2021-09-15 15:59:55 +0200

    [TAO-634] Fixed incorrect group by clause in vehiicle query

--- api/data/vehicle.sql
+++ api/data/vehicle.sql
@@ -4,8 +4,8 @@ select
     case merk when null then null else md5(merk) end as merk,
     case inrichting when null then null else md5(inrichting) end as inrichting,
     datum_eerste_toelating,
-    NULL,  -- datum_tenaamstelling
-    case toegestane_maximum_massa_voertuig when null then null else md5(toegestane_maximum_massa_voertuig) end as toegestane_maximum_massa_voertuig,
+    NULL as datum_tenaamstelling,  -- datum_tenaamstelling
+    toegestane_maximum_massa_voertuig,
     case europese_voertuigcategorie when null then null else md5(europese_voertuigcategorie) end as europese_voertuigcategorie,
     case europese_voertuigcategorie_toevoeging when null then null else md5(europese_voertuigcategorie_toevoeging) end as europese_voertuigcategorie_toevoeging,
     case when taxi_indicator then 'TRUE' else 'FALSE' end as taxi_indicator,
@@ -24,7 +24,6 @@ group by
     merk,
     inrichting,
     datum_eerste_toelating,
-    NULL,  -- datum_tenaamstelling
     toegestane_maximum_massa_voertuig,
     europese_voertuigcategorie,
     europese_voertuigcategorie_toevoeging,
