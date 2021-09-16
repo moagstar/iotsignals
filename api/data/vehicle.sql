b7fd66514174eea1d1b363ba3c284c17ab7150de
Daniel Bradburn <moagstar@gmail.com>
2021-09-16 11:39:00 +0200

    [TAO-634] Removed superfluous CASE WHEN in vehicle selection script

--- api/data/vehicle.sql
+++ api/data/vehicle.sql
@@ -1,13 +1,13 @@
 select
-    case kenteken_land when null then null else md5(kenteken_land) end as kenteken_land,
-    case voertuig_soort when null then null else md5(voertuig_soort) end as voertuig_soort,
-    case merk when null then null else md5(merk) end as merk,
-    case inrichting when null then null else md5(inrichting) end as inrichting,
+    md5(kenteken_land) as kenteken_land,
+    md5(voertuig_soort) as voertuig_soort,
+    md5(merk) as merk,
+    md5(inrichting) as inrichting,
     datum_eerste_toelating,
     NULL as datum_tenaamstelling,  -- datum_tenaamstelling
     toegestane_maximum_massa_voertuig,
-    case europese_voertuigcategorie when null then null else md5(europese_voertuigcategorie) end as europese_voertuigcategorie,
-    case europese_voertuigcategorie_toevoeging when null then null else md5(europese_voertuigcategorie_toevoeging) end as europese_voertuigcategorie_toevoeging,
+    md5(europese_voertuigcategorie) as europese_voertuigcategorie,
+    md5(europese_voertuigcategorie_toevoeging) as europese_voertuigcategorie_toevoeging,
     case when taxi_indicator then 'TRUE' else 'FALSE' end as taxi_indicator,
     maximale_constructie_snelheid_bromsnorfiets,
     brandstoffen,
@@ -15,7 +15,7 @@ select
     diesel,
     gasoline,
     electric,
-    case versit_klasse when null then null else md5(versit_klasse) end as versit_klasse,
+    md5(versit_klasse) as versit_klasse,
     count(*)
 from passage_passage_20210901
 group by
